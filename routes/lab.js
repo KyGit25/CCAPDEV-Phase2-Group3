59842e6bbd88414f095ec2dc45aed4b3676348e8
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-07-14 23:19:29 +0800

    Update lab.js

--- routes/lab.js
+++ routes/lab.js
@@ -6,6 +6,7 @@ router.get('/', labController.listLabs);
 router.get('/list', labController.listLabs);
 
 router.get('/:id/availability', labController.getLabAvailability);
+router.get('/availability/:id', labController.getLabAvailability);
 
 module.exports = router;
 
