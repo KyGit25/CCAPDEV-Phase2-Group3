a5de910c87eeea957d39eb48a14d7d4c26f8b4da
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-07-14 22:13:12 +0800

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/searchController');
 
-// Search available slots
 router.get('/slots', searchController.searchFreeSlots);
 
-// Search users
 router.get('/users', searchController.searchUsers);
 
 module.exports = router;
